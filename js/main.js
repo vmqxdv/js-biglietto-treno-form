74d7e1cacb27faffaf5b825410264f4b01d18868
vmqxdv <vquintilio@gmail.com>
2025-03-05 14:35:17 +0100

    feat: Dichiarata costante form

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale
+del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
+*/
+
+const inputForm = document.getElementById('ticket-price');
+
+console.log(inputForm);
